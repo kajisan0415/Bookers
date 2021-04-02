131bfbf26052db3e52826c61bbd1addf631ff1b8
kajisan0415 <agonagakajisan0415@gmail.com>
2021-04-02 08:12:16 +0000

    create index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,17 @@
 class BooksController < ApplicationController
 
   def index
+    @books = Book.all
   end
 
   def show
   end
 
   def new
+    @book = Book.new
+  end
+  
+  def create
   end
 
   def edit
