39d36743eafa31986488ff5965a6a4956fcb4ac8
kajisan0415 <agonagakajisan0415@gmail.com>
2021-04-03 12:56:03 +0000

    Create function

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,19 +6,34 @@ class BooksController < ApplicationController
   end
 
   def show
+    @book = Book.find(params[:id])
   end
   
   def new
-    @book = Book.new
+    
   end
   
   def create
     @book = Book.new(book_params)
     @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
   end
 
   def edit
+    @book = Book.find(params[:id])
+  
+  end
+  
+  def update
+  @book = Book.find(params[:id])
+  @book.update(book_params)
+  redirect_to book_path(@book.id)
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to "/books"
   end
   
   private
