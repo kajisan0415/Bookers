872bca0e736903ccc6d20f91227b2653332ff8b1
kajisan0415 <agonagakajisan0415@gmail.com>
2021-04-04 13:15:50 +0000

    Add Validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,8 +15,13 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new(book_params)
-    @book.save
+     if @book.save
+      flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
+     else
+       @books = Book.all 
+       render("books/index")
+     end
   end
 
   def edit
@@ -26,13 +31,18 @@ class BooksController < ApplicationController
   
   def update
   @book = Book.find(params[:id])
-  @book.update(book_params)
-  redirect_to book_path(@book.id)
+   if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+   redirect_to book_path(@book.id)
+   else
+   render("books/edit")
+   end
   end
   
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to "/books"
   end
   
